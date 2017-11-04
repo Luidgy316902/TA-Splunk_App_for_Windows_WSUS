b843dd471f1e3510fc60707aeeb0bf3329290696
TraGicCode <6224270+TraGicCode@users.noreply.github.com>
2017-11-04 18:22:51 -0500

    Initial Commit

---
+++ .Build.ps1
@@ -0,0 +1,37 @@
+<#
+.Synopsis
+    Build Script
+#>
+
+[CmdletBinding()]
+Param(
+)
+
+Set-StrictMode -Version Latest
+
+# Includes
+
+# Script Variables
+$BuildArtifactsFolder = "$BuildRoot\.buildartifacts"
+
+#=================================================================================================
+# Synopsis: Performs a Clean of all build artifacts
+#=================================================================================================
+Task Clean {
+    If (Test-Path -Path $BuildArtifactsFolder)
+	{
+		Remove-Item $BuildArtifactsFolder -Force -Recurse
+	}
+
+    New-Item -Path $BuildArtifactsFolder -ItemType Directory | Out-Null
+}
+
+#=================================================================================================
+# Synopsis: Package Splunk App for uploading
+#=================================================================================================
+Task Package {
+    & 7z.exe a -ttar -xr!"$BuildArtifactsFolder" -xr!".Build.ps1" -xr!"Invoke-Build.ps1" "$BuildArtifactsFolder\TA-Splunk_App_for_Windows_WSUS.tar" .
+}
+
+Task . Clean, Package, {
+}
